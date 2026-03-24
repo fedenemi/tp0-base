0a0b8fd8d5374cd73649ba9333159be1322a0e27
fedenemi <fedenemi@gmail.com>
2026-03-24 17:30:57 -0300

    fix server main and restore initialize_log function

--- server/main.py
+++ server/main.py
@@ -33,6 +33,14 @@ def initialize_config():
     return config_params
 
 
+def initialize_log(logging_level):
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging_level,
+        datefmt='%Y-%m-%d %H:%M:%S',
+    )
+
+
 def main():
     config_params = initialize_config()
     logging_level = config_params["logging_level"]
@@ -47,36 +55,6 @@ def main():
 
     server = Server(port, listen_backlog, agencies_amount)
     server.run()
-
-def main():
-    config_params = initialize_config()
-    logging_level = config_params["logging_level"]
-    port = config_params["port"]
-    listen_backlog = config_params["listen_backlog"]
-
-    initialize_log(logging_level)
-
-    # Log config parameters at the beginning of the program to verify the configuration
-    # of the component
-    logging.debug(f"action: config | result: success | port: {port} | "
-                  f"listen_backlog: {listen_backlog} | logging_level: {logging_level}")
-
-    # Initialize server and start server loop
-    server = Server(port, listen_backlog)
-    server.run()
-
-def initialize_log(logging_level):
-    """
-    Python custom logging initialization
-
-    Current timestamp is added to be able to identify in docker
-    compose logs the date when the log has arrived
-    """
-    logging.basicConfig(
-        format='%(asctime)s %(levelname)-8s %(message)s',
-        level=logging_level,
-        datefmt='%Y-%m-%d %H:%M:%S',
-    )
 
 
 if __name__ == "__main__":
